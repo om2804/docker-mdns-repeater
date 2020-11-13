8f0af9ba8201ccb1f485bb21f9fd7afd994f88c3
Thorrak <thorrak@excelinspace.com>
2020-11-13 13:29:21 -0500

    Clean-up and update README.md

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
 # docker-mdns-repeater
-Allow docker containers in virtual networks to send/receive mdns broadcast messages
 
-Original sources from https://bitbucket.org/geekman/mdns-repeater/raw/28ecc2ab9a0e26c73148711c867d9d2b5dafff91/mdns-repeater.c
+This image uses Darell Tan's mdns-repeater to bridge/repeat mDNS requests between two network interfaces. 
+
+The intended use of this container is to allow a docker-compose stack running in `net=bridge` mode to be able to communicate with the Docker host's external network.
+
+
+## Using this container
+
+This container was designed to be used as part of a docker-compose stack. When creating the configuration file:
+
+1. Use docker-compose version 3.5 or later
+2. Ensure that each of the bridged containers are part of the same, named network
+3. Ensure that the network is named in the `networks:` section
+4. Create & link an environment variable file similar to [env.sample](env.sample) to your mdns_reflector image (or set the appropriate environment variables in the stack's configuration)
+
+See `mdns_reflector` in the example docker-compose file below for the other attributes necessary to run the container.
+
+There are three environment variables that must be set:
+- **EXTERNAL_INTERFACE** - The interface name of the external interface we want to bridge (e.g. `eth0`, `wlan0`, etc.)
+- **DOCKER_NETWORK_NAME** - The name of the named network in the stack (`walled` in the example below)
+
+
+
+### Example docker-compose file
+
+```
+version: '3.5'
+
+services:
+  redis:
+    image: redis:5.0
+    networks:
+      - walled
+
+  nginx:
+    image: nginx
+    networks:
+      - walled
+
+  mdns_reflector:
+    image: jdbeeler/mdns-repeater:latest
+    network_mode: "host"
+    privileged: true
+    env_file:
+      - ./envs/mdns-reflector
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock
+
+networks:
+  walled:
+    name: walled
+```
+
+
+## Credits
+
+mdns-repeater.c was obtained from [kennylevinsen's fork](https://github.com/kennylevinsen/mdns-repeater) of [Darell Tan's](https://bitbucket.org/geekman/mdns-repeater) mdns-repeater.c
+
+The original dockerization of mdns-repeater was done by [angelnu](https://github.com/angelnu/docker-mdns_repeater) 
+
+Licensing is GPLv2 as inherited from Darell Tan's mdns-repeater.c.
+
+
+
--- mdns-repeater.c
+++ mdns-repeater.c
@@ -32,12 +32,18 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <net/if.h>
+#include <errno.h>
 
 #define PACKAGE "mdns-repeater"
 #define MDNS_ADDR "224.0.0.251"
 #define MDNS_PORT 5353
 
+#ifndef PIDFILE
 #define PIDFILE "/var/run/" PACKAGE ".pid"
+#endif
+
+#define MAX_SOCKS 16
+#define MAX_SUBNETS 16
 
 struct if_sock {
 	const char *ifname;		/* interface name  */
@@ -47,17 +53,27 @@ struct if_sock {
 	struct in_addr net;		/* interface network (computed) */
 };
 
+struct subnet {
+	struct in_addr addr;    /* subnet addr */
+	struct in_addr mask;    /* subnet mask */
+	struct in_addr net;     /* subnet net (computed) */
+};
+
 int server_sockfd = -1;
 
 int num_socks = 0;
-struct if_sock socks[5];
+struct if_sock socks[MAX_SOCKS];
+
+int num_blacklisted_subnets = 0;
+struct subnet blacklisted_subnets[MAX_SUBNETS];
+
+int num_whitelisted_subnets = 0;
+struct subnet whitelisted_subnets[MAX_SUBNETS];
 
 #define PACKET_SIZE 65536
 void *pkt_data = NULL;
 
-typedef enum { false, true } bool;
-bool foreground = false;
-bool verbose = false;
+int foreground = 0;
 int shutdown_flag = 0;
 
 char *pid_file = PIDFILE;
@@ -81,7 +97,7 @@ void log_message(int loglevel, char *fmt_str, ...) {
 static int create_recv_sock() {
 	int sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sd < 0) {
-		log_message(LOG_ERR, "recv socket(): %m");
+		log_message(LOG_ERR, "recv socket(): %s", strerror(errno));
 		return sd;
 	}
 
@@ -89,7 +105,7 @@ static int create_recv_sock() {
 
 	int on = 1;
 	if ((r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0) {
-		log_message(LOG_ERR, "recv setsockopt(SO_REUSEADDR): %m");
+		log_message(LOG_ERR, "recv setsockopt(SO_REUSEADDR): %s", strerror(errno));
 		return r;
 	}
 
@@ -100,18 +116,18 @@ static int create_recv_sock() {
 	serveraddr.sin_port = htons(MDNS_PORT);
 	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);	/* receive multicast */
 	if ((r = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0) {
-		log_message(LOG_ERR, "recv bind(): %m");
+		log_message(LOG_ERR, "recv bind(): %s", strerror(errno));
 	}
 
 	// enable loopback in case someone else needs the data
 	if ((r = setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, &on, sizeof(on))) < 0) {
-		log_message(LOG_ERR, "recv setsockopt(IP_MULTICAST_LOOP): %m");
+		log_message(LOG_ERR, "recv setsockopt(IP_MULTICAST_LOOP): %s", strerror(errno));
 		return r;
 	}
 
 #ifdef IP_PKTINFO
 	if ((r = setsockopt(sd, SOL_IP, IP_PKTINFO, &on, sizeof(on))) < 0) {
-		log_message(LOG_ERR, "recv setsockopt(IP_PKTINFO): %m");
+		log_message(LOG_ERR, "recv setsockopt(IP_PKTINFO): %s", strerror(errno));
 		return r;
 	}
 #endif
@@ -122,7 +138,7 @@ static int create_recv_sock() {
 static int create_send_sock(int recv_sockfd, const char *ifname, struct if_sock *sockdata) {
 	int sd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sd < 0) {
-		log_message(LOG_ERR, "send socket(): %m");
+		log_message(LOG_ERR, "send socket(): %s", strerror(errno));
 		return sd;
 	}
 
@@ -138,7 +154,7 @@ static int create_send_sock(int recv_sockfd, const char *ifname, struct if_sock 
 
 #ifdef SO_BINDTODEVICE
 	if ((r = setsockopt(sd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(struct ifreq))) < 0) {
-		log_message(LOG_ERR, "send setsockopt(SO_BINDTODEVICE): %m");
+		log_message(LOG_ERR, "send setsockopt(SO_BINDTODEVICE): %s", strerror(errno));
 		return r;
 	}
 #endif
@@ -158,7 +174,7 @@ static int create_send_sock(int recv_sockfd, const char *ifname, struct if_sock 
 
 	int on = 1;
 	if ((r = setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on))) < 0) {
-		log_message(LOG_ERR, "send setsockopt(SO_REUSEADDR): %m");
+		log_message(LOG_ERR, "send setsockopt(SO_REUSEADDR): %s", strerror(errno));
 		return r;
 	}
 
@@ -169,8 +185,14 @@ static int create_send_sock(int recv_sockfd, const char *ifname, struct if_sock 
 	serveraddr.sin_port = htons(MDNS_PORT);
 	serveraddr.sin_addr.s_addr = if_addr->s_addr;
 	if ((r = bind(sd, (struct sockaddr *)&serveraddr, sizeof(serveraddr))) < 0) {
-		log_message(LOG_ERR, "send bind(): %m");
+		log_message(LOG_ERR, "send bind(): %s", strerror(errno));
 	}
+
+#if __FreeBSD__
+	if((r = setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, &serveraddr.sin_addr, sizeof(serveraddr.sin_addr))) < 0) {
+		log_message(LOG_ERR, "send ip_multicast_if(): errno %d: %s", errno, strerror(errno));
+	}
+#endif
 
 	// add membership to receiving socket
 	struct ip_mreq mreq;
@@ -178,13 +200,13 @@ static int create_send_sock(int recv_sockfd, const char *ifname, struct if_sock 
 	mreq.imr_interface.s_addr = if_addr->s_addr;
 	mreq.imr_multiaddr.s_addr = inet_addr(MDNS_ADDR);
 	if ((r = setsockopt(recv_sockfd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq))) < 0) {
-		log_message(LOG_ERR, "recv setsockopt(IP_ADD_MEMBERSHIP): %m");
+		log_message(LOG_ERR, "recv setsockopt(IP_ADD_MEMBERSHIP): %s", strerror(errno));
 		return r;
 	}
 
 	// enable loopback in case someone else needs the data
 	if ((r = setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, &on, sizeof(on))) < 0) {
-		log_message(LOG_ERR, "send setsockopt(IP_MULTICAST_LOOP): %m");
+		log_message(LOG_ERR, "send setsockopt(IP_MULTICAST_LOOP): %s", strerror(errno));
 		return r;
 	}
 
@@ -251,7 +273,7 @@ static void daemonize() {
 	pid_t running_pid;
 	pid_t pid = fork();
 	if (pid < 0) {
-		log_message(LOG_ERR, "fork(): %m");
+		log_message(LOG_ERR, "fork(): %s", strerror(errno));
 		exit(1);
 	}
 
@@ -300,22 +322,74 @@ static void show_help(const char *progname) {
 					"maximum number of interfaces is 5\n"
 					"\n"
 					" flags:\n"
-					"	-f	runs in foreground and prints logs to stdout\n"
-          "	-v	verbose mode. Trace messages (very verbose!)\n"
+					"	-f	runs in foreground for debugging\n"
+					"	-b	blacklist subnet (eg. 192.168.1.1/24)\n"
+					"	-w	whitelist subnet (eg. 192.168.1.1/24)\n"
 					"	-p	specifies the pid file path (default: " PIDFILE ")\n"
 					"	-h	shows this help\n"
 					"\n"
 		);
 }
 
+int parse(char *input, struct subnet *s) {
+	int delim = 0;
+	int end = 0;
+	while (input[end] != 0) {
+		if (input[end] == '/') {
+			delim = end;
+		}
+		end++;
+	}
+
+	if (end == 0 || delim == 0 || end == delim) {
+		return -1;
+	}
+
+	char *addr = (char*) malloc(end);
+
+	memset(addr, 0, end);
+	strncpy(addr, input, delim);
+	if (inet_pton(AF_INET, addr, &s->addr) != 1) {
+		free(addr);
+		return -2;
+	}
+
+	memset(addr, 0, end);
+	strncpy(addr, input+delim+1, end-delim-1);
+	int mask = atoi(addr);
+	free(addr);
+
+	if (mask < 0 || mask > 32) {
+		return -3;
+	}
+
+	s->mask.s_addr = ntohl((uint32_t)0xFFFFFFFF << (32 - mask));
+	s->net.s_addr = s->addr.s_addr & s->mask.s_addr;
+
+	return 0;
+}
+
+int tostring(struct subnet *s, char* buf, int len) {
+	char *addr_str = strdup(inet_ntoa(s->addr));
+	char *mask_str = strdup(inet_ntoa(s->mask));
+	char *net_str = strdup(inet_ntoa(s->net));
+	int l = snprintf(buf, len, "addr %s mask %s net %s", addr_str, mask_str, net_str);
+	free(addr_str);
+	free(mask_str);
+	free(net_str);
+
+	return l;
+}
+
 static int parse_opts(int argc, char *argv[]) {
-	int c;
+	int c, res;
 	int help = 0;
-	while ((c = getopt(argc, argv, "hfp:")) != -1) {
+	struct subnet *ss;
+	char *msg;
+	while ((c = getopt(argc, argv, "hfp:b:w:")) != -1) {
 		switch (c) {
 			case 'h': help = 1; break;
-			case 'f': foreground = true; break;
-      case 'v': verbose = true; break;
+			case 'f': foreground = 1; break;
 			case 'p':
 				if (optarg[0] != '/')
 					log_message(LOG_ERR, "pid file path must be absolute");
@@ -323,6 +397,72 @@ static int parse_opts(int argc, char *argv[]) {
 					pid_file = optarg;
 				break;
 
+			case 'b':
+				if (num_blacklisted_subnets >= MAX_SUBNETS) {
+					log_message(LOG_ERR, "too many blacklisted subnets (maximum is %d)", MAX_SUBNETS);
+					exit(2);
+				}
+
+				if (num_whitelisted_subnets != 0) {
+					log_message(LOG_ERR, "simultaneous whitelisting and blacklisting does not make sense");
+					exit(2);
+				}
+
+				ss = &blacklisted_subnets[num_blacklisted_subnets];
+				res = parse(optarg, ss);
+				switch (res) {
+					case -1:
+						log_message(LOG_ERR, "invalid blacklist argument");
+						exit(2);
+					case -2:
+						log_message(LOG_ERR, "could not parse netmask");
+						exit(2);
+					case -3:
+						log_message(LOG_ERR, "invalid netmask");
+						exit(2);
+				}
+
+				num_blacklisted_subnets++;
+
+				msg = malloc(128);
+				memset(msg, 0, 128);
+				tostring(ss, msg, 128);
+				log_message(LOG_INFO, "blacklist %s", msg);
+				free(msg);
+				break;
+			case 'w':
+				if (num_whitelisted_subnets >= MAX_SUBNETS) {
+					log_message(LOG_ERR, "too many whitelisted subnets (maximum is %d)", MAX_SUBNETS);
+					exit(2);
+				}
+
+				if (num_blacklisted_subnets != 0) {
+					log_message(LOG_ERR, "simultaneous whitelisting and blacklisting does not make sense");
+					exit(2);
+				}
+
+				ss = &whitelisted_subnets[num_whitelisted_subnets];
+				res = parse(optarg, ss);
+				switch (res) {
+					case -1:
+						log_message(LOG_ERR, "invalid whitelist argument");
+						exit(2);
+					case -2:
+						log_message(LOG_ERR, "could not parse netmask");
+						exit(2);
+					case -3:
+						log_message(LOG_ERR, "invalid netmask");
+						exit(2);
+				}
+
+				num_whitelisted_subnets++;
+
+				msg = malloc(128);
+				memset(msg, 0, 128);
+				tostring(ss, msg, 128);
+				log_message(LOG_INFO, "whitelist %s", msg);
+				free(msg);
+				break;
 			case '?':
 			case ':':
 				fputs("\n", stderr);
@@ -378,6 +518,11 @@ int main(int argc, char *argv[]) {
 	// create sending sockets
 	int i;
 	for (i = optind; i < argc; i++) {
+		if (num_socks >= MAX_SOCKS) {
+			log_message(LOG_ERR, "too many sockets (maximum is %d)", MAX_SOCKS);
+			exit(2);
+		}
+
 		int sockfd = create_send_sock(server_sockfd, argv[i], &socks[num_socks]);
 		if (sockfd < 0) {
 			log_message(LOG_ERR, "unable to create socket for interface %s", argv[i]);
@@ -389,7 +534,7 @@ int main(int argc, char *argv[]) {
 
 	pkt_data = malloc(PACKET_SIZE);
 	if (pkt_data == NULL) {
-		log_message(LOG_ERR, "cannot malloc() packet buffer: %m");
+		log_message(LOG_ERR, "cannot malloc() packet buffer: %s", strerror(errno));
 		r = 1;
 		goto end_main;
 	}
@@ -413,7 +558,7 @@ int main(int argc, char *argv[]) {
 			ssize_t recvsize = recvfrom(server_sockfd, pkt_data, PACKET_SIZE, 0,
 				(struct sockaddr *) &fromaddr, &sockaddr_size);
 			if (recvsize < 0) {
-				log_message(LOG_ERR, "recv(): %m");
+				log_message(LOG_ERR, "recv(): %s", strerror(errno));
 			}
 
 			int j;
@@ -429,24 +574,56 @@ int main(int argc, char *argv[]) {
 			if (self_generated_packet)
 				continue;
 
-			if (verbose)
-				printf("data from=%s size=%ld\n", inet_ntoa(fromaddr.sin_addr), recvsize);
+			if (num_whitelisted_subnets != 0) {
+				char whitelisted_packet = 0;
+				for (j = 0; j < num_whitelisted_subnets; j++) {
+					// check for whitelist
+					if ((fromaddr.sin_addr.s_addr & whitelisted_subnets[j].mask.s_addr) == whitelisted_subnets[j].net.s_addr) {
+						whitelisted_packet = 1;
+						break;
+					}
+				}
+
+				if (!whitelisted_packet) {
+					if (foreground)
+						printf("skipping packet from=%s size=%zd\n", inet_ntoa(fromaddr.sin_addr), recvsize);
+					continue;
+				}
+			} else {
+				char blacklisted_packet = 0;
+				for (j = 0; j < num_blacklisted_subnets; j++) {
+					// check for blacklist
+					if ((fromaddr.sin_addr.s_addr & blacklisted_subnets[j].mask.s_addr) == blacklisted_subnets[j].net.s_addr) {
+						blacklisted_packet = 1;
+						break;
+					}
+				}
+
+				if (blacklisted_packet) {
+					if (foreground)
+						printf("skipping packet from=%s size=%zd\n", inet_ntoa(fromaddr.sin_addr), recvsize);
+					continue;
+				}
+			}
+
+			if (foreground)
+				printf("data from=%s size=%zd\n", inet_ntoa(fromaddr.sin_addr), recvsize);
 
 			for (j = 0; j < num_socks; j++) {
 				// do not repeat packet back to the same network from which it originated
 				if ((fromaddr.sin_addr.s_addr & socks[j].mask.s_addr) == socks[j].net.s_addr)
 					continue;
 
-				if (verbose)
+				if (foreground)
 					printf("repeating data to %s\n", socks[j].ifname);
 
 				// repeat data
 				ssize_t sentsize = send_packet(socks[j].sockfd, pkt_data, (size_t) recvsize);
 				if (sentsize != recvsize) {
 					if (sentsize < 0)
-						log_message(LOG_ERR, "send()");
+						log_message(LOG_ERR, "send(): %s", strerror(errno));
 					else
-						log_message(LOG_ERR, "send_packet size differs: sent=%ld actual=%ld",
+						log_message(LOG_ERR, "send_packet size differs: sent=%zd actual=%zd",
 							recvsize, sentsize);
 				}
 			}
